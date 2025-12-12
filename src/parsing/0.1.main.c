4d78d2e1baaf4b5378e7f17fc175e4a4381d2c55
kristin0schmitt <kristin.schmitt@posteo.de>
2025-12-12 12:35:44 +0100

    done. ready for testing.

--- src/parsing/0.1.main.c
+++ src/parsing/0.1.main.c
@@ -3,32 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   0.1.main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aidarsharafeev <aidarsharafeev@student.    +#+  +:+       +#+        */
+/*   By: kschmitt <kschmitt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 15:34:19 by kschmitt          #+#    #+#             */
-/*   Updated: 2025/12/10 18:34:14 by aidarsharaf      ###   ########.fr       */
+/*   Updated: 2025/12/12 12:33:20 by kschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// Structure logic
-// 0.x. overall
-// 1.x. pipeline level (input str)
-// 2.x. command string level (input str split by pipes)
-// 3.x. token level (arg vs. redir)
-// 4.x. argument level (cmd, built-in cmd, flag, arg, env. arg, quotes)
-
-// works
 int	main(int ac, char **av, char **env)
 {
-	t_shell	*shell;
+	t_shell	*minishell;
+
 	(void)ac;
 	(void)av;
-
-	shell = malloc(sizeof(t_shell));
-	shell->env = ft_env_dup(env);
-	if (!init_minishell(env))
-		return (printf("Error with initialization.\n"), 1);
+	minishell = (t_shell *)malloc(sizeof(t_shell));
+	if (!minishell)
+		return (printf("Memory allocation failed.\n"), FAILURE);
+	minishell->env = ft_env_dup(env);
+	init_minishell(minishell);
+	end_minishell(minishell);
+	rl_clear_history();
 	return (0);
 }
